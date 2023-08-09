2f9ee1ce84568aead2d5460aaa7d71071112fdb7
Vitalik <91615314+Vitalik001@users.noreply.github.com>
2023-08-09 17:43:21 +0300

    added async but need fixes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import asyncio
 import requests
+from threading import Lock
 
+lock = Lock()
 current_downloads = 0
+
+
 # Implement asynchronous(using asyncio)
 # command-line file downloader.
 # The command line should read the file
@@ -26,34 +30,38 @@ current_downloads = 0
 # All files finished downloading
 
 
-
-async def download_url():
+async def download_url(url):
     global current_downloads
-    # url = input("Please enter the file url to download:")
-    url = "https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg"
-    response = requests.get(url)
+    response = asyncio.create_task(requests.get(url))
     source, name = url.split("/")[-2:]
+    await response
     if response.status_code == 200:
 
         print(f"Started downloading a file from {source}")
-        current_downloads+=1
+        with lock:
+            current_downloads += 1
         print(f"Downloading {current_downloads} files")
 
         with open(name, "wb") as file:
             file.write(response.content)
 
         print("File downloaded successfully.")
-        current_downloads-=1
+        with lock:
+            current_downloads -= 1
     else:
         print(f"Failed to download file from {source}. Status code: {response.status_code}")
 
-
+    if current_downloads == 0:
+        print("All files finished downloading")
 
 
 async def main():
-    await download_url()
-    print("All files finished downloading")
+    print("Please enter the file url to download:")
+    # url = "https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg"
+    tasks = []
+    while (url := input()) != "exit":
+        tasks.append(asyncio.create_task(download_url(url)))
+    await asyncio.gather(*tasks)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     asyncio.run(main())
-
