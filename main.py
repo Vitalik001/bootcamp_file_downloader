15ed2299bfcac9f48f294ff2f394b2e21df9730c
Vitalik <91615314+Vitalik001@users.noreply.github.com>
2023-08-09 19:16:10 +0300

    wip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import asyncio
-import requests
-from threading import Lock
+import aiohttp
 
-lock = Lock()
 current_downloads = 0
 
 
@@ -30,38 +28,64 @@ current_downloads = 0
 # All files finished downloading
 
 
-async def download_url(url):
+# async def create_task(url):
+#
+#
+#     await response
+#     if response.status_code == 200:
+#
+#         print(f"Started downloading a file from {source}")
+#
+#         current_downloads += 1
+#         print(f"Downloading {current_downloads} files")
+#
+#         with open(name, "wb") as file:
+#             file.write(response.content)
+#
+#         print("File downloaded successfully.")
+#         current_downloads -= 1
+#     else:
+#         print(f"Failed to download file from {source}. Status code: {response.status_code}")
+#
+#     if current_downloads == 0:
+#         print("All files finished downloading")
+
+async def handle_response(response, source, name):
     global current_downloads
-    response = asyncio.create_task(requests.get(url))
-    source, name = url.split("/")[-2:]
-    await response
-    if response.status_code == 200:
+    print(f"Started downloading a file from {source}")
 
-        print(f"Started downloading a file from {source}")
-        with lock:
-            current_downloads += 1
-        print(f"Downloading {current_downloads} files")
+    current_downloads += 1
+    print(f"Downloading {current_downloads} files")
 
-        with open(name, "wb") as file:
-            file.write(response.content)
+    with open(name, "wb") as file:
+        async for chunk in response.content.iter_any():
+            file.write(chunk)
 
-        print("File downloaded successfully.")
-        with lock:
-            current_downloads -= 1
-    else:
-        print(f"Failed to download file from {source}. Status code: {response.status_code}")
+    print("File downloaded successfully.")
+    current_downloads -= 1
 
-    if current_downloads == 0:
-        print("All files finished downloading")
 
+if current_downloads == 0:
+    print("All files finished downloading")
 
 async def main():
     print("Please enter the file url to download:")
     # url = "https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg"
-    tasks = []
-    while (url := input()) != "exit":
-        tasks.append(asyncio.create_task(download_url(url)))
-    await asyncio.gather(*tasks)
+    async with aiohttp.ClientSession() as session:
+
+        while (url := input()) != "exit":
+            source, name = url.split("/")[-2:]
+
+            response = await session.get(url, ssl=False)
+
+            if response.status == 200:
+
+                await handle_response(response, source, name)
+
+            else:
+                print(f"Failed to download file from {source}. Status code: {response.status}")
+
+    # await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
